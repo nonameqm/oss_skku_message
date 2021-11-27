b598fa44689f79016dfe98e5795bfa18f40bb907
nonameqm <jwun56@gmail.com>
2021-11-27 21:01:27 +0900

    Content Box Created

--- app/js/main_page.js
+++ app/js/main_page.js
@@ -33,9 +33,45 @@ btnlist.forEach((button)=>{
 
 button.onclick=function(){
   //keyword_setting pop up
-
-  
+ 
 }
 
 
 
+document.addEventListener('DOMContentLoaded', () => {
+  $('.popup_box').hide();
+
+  $('#keyword_button').click(function(){
+    popup_show()
+  })
+  $('#close_button').click(function () {
+    popup_hide();
+  })
+})
+
+
+
+
+
+function popup_show() {
+  var obj = document.createElement("div");
+  with (obj.style) {
+    left = 0;
+    top = 0;
+    width = "100%";
+    height = "100%";
+    backgroundColor = "#000";
+    filter = "Alpha(Opacity=50)";
+    opacity = "0.6";
+    position = "absolute";
+    zIndex = "10000";
+  }
+  obj.id = "iframeBg"
+  document.body.appendChild(obj);
+  $('.popup_box').show();
+}
+
+function popup_hide() {
+  $('#iframeBg').remove();
+  $('.popup_box').hide();
+}
