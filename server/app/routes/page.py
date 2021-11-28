d9697ad0b3db81fbae7960bb5dcb79b1d02e780d
YoungJaeBae <qodudwos@gmail.com>
2021-11-28 15:16:45 +0900

    carry user info in mainpage

--- server/app/routes/page.py
+++ server/app/routes/page.py
@@ -1,32 +1,6 @@
-from fastapi import APIRouter, Request
-from fastapi.templating import Jinja2Templates
-
-
-
-templates = Jinja2Templates(directory="templates")
-router=APIRouter(prefix="/page")
-
-
-###########################################rendering##############################################
-@router.get("/login")
-def login_render(request:Request):
-    return templates.TemplateResponse('pages/login.html', context = {'request':request})
-
-@router.get("/checkin")
-def checkin_render(request:Request):
-    return templates.TemplateResponse('pages/checkin.html', context = {'request':request})
-
-@router.get("/main/{uid}")
-def checkin_render(request:Request, uid:str):
-    return templates.TemplateResponse('pages/main_page.html', context = {'request':request, 'user':uid})
-
-@router.get("/admin")
-def checkin_render(request:Request):
-    return templates.TemplateResponse('pages/admin.html', context = {'request':request})
-
-###########################################rendering##############################################
 
 from fastapi import APIRouter, Request,  Depends
+from fastapi.exceptions import HTTPException
 from fastapi.templating import Jinja2Templates
 from database.database import SessionLocal
 from sqlalchemy.orm import Session
@@ -54,18 +28,21 @@ def login_render(request:Request):
 def checkin_render(request:Request):
     return templates.TemplateResponse('pages/checkin.html', context = {'request':request})
 
-@router.get("/main")
-def checkin_render(request:Request, db: Session = Depends(get_database_session)):
+@router.get("/main/{uid}")
+def checkin_render(request:Request, uid:str,db: Session = Depends(get_database_session)):
     message_list = crud.get_msgs(db=db)
+    keywords = crud.get_keyword_by_uid(db = db, uid = uid)
     context={
         'request': request, 
-        'data': message_list
+        'data': message_list,
+        'user': uid,
+        'keywords':keywords
     }
     return templates.TemplateResponse('pages/main_page.html', context = context)
 
 @router.get("/admin")
 def checkin_render(request:Request):
     return templates.TemplateResponse('pages/admin.html', context = {'request':request})
-
+    
 ###########################################rendering##############################################
 
